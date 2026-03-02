d4657cde050b007d02075833fe07c4c3f900de50
AONE8 <malyshko.dp@gmail.com>
2026-03-02 18:03:53 +0200

    Change user agent

--- utils/initializePageForScrapping.js
+++ utils/initializePageForScrapping.js
@@ -1,9 +1,8 @@
-import { exec } from "child_process";
 import puppeteer from "puppeteer";
 
 const NAVIGATION_TIMEOUT_MS = 120000;
 const USER_AGENT =
-  "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
+  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36";
 
 async function initializePageForScrapping(url) {
   let browser;
@@ -18,6 +17,7 @@ async function initializePageForScrapping(url) {
         "--disable-dev-shm-usage",
         "--disable-gpu",
       ],
+      ignoreDefaultArgs: ["--disable-extensions"],
     });
 
     const page = await browser.newPage();
